de02fd4c34029a3a0376c5c82d851921f63b91c9
Andreas Fridlund <afrxx09@student.lnu.se>
2013-11-12 14:33:47 +0100

    Skapat solution och unit test

---
+++ TriangleUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleUnitTest
+{
+    [TestClass]
+    public class TriUnitTest
+    {
+        [TestMethod]
+        public void isIsoscelesTest()
+        {
+            Triangle tri = new Triangle(1.1, 2.1, 1.1);
+            Assert.IsTrue(tri.isIsosceles());
+        }
+    }
+}
